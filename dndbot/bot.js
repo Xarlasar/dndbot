04ef3e4bf93b2f2f44a9534e91c48a4618297d0d
Xarlasar <xarlasar12@gmail.com>
2018-03-08 18:42:43 -0500

    added test cmd

--- dndbot/bot.js
+++ dndbot/bot.js
@@ -20,10 +20,10 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
+    if (message.substring(0, 1) == '.') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-       
+        
         args = args.splice(1);
         switch(cmd) {
             // !ping
@@ -31,6 +31,12 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 bot.sendMessage({
                     to: channelID,
                     message: 'Pong!'
+                });
+            break;
+            case 'big gay':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'no u'
                 });
             break;
             // Just add any case commands if you want to..
